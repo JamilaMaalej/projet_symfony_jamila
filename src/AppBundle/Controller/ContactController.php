6468a54f1aa6a23a52cd415b72ef9f3e0b89d859
formation <formation>
2016-02-01 12:03:26 +0100

    [WIP]Added slave branch form

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ContactController
+{
+	
+  /**
+    *@Template
+    */
+    public function contactAction()
+    {
+    	
+        return array() ;
+    }
+
+ }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -31,4 +31,6 @@ class DefaultController extends Controller
     {
         return $this->render('AppBundle:default:posts.html.twig');
     }
+
+  
 }
